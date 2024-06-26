85a63b6b73d6d05d1b5e4ce752775fbd1400c3ee
Morris Nkosi <Morris@mfundopedia.co.za>
2024-06-26 08:11:05 +0200

    finding sentence with the maximum word count

--- TS.TechnicalTest/LongestSentanceAnswer.cs
+++ TS.TechnicalTest/LongestSentanceAnswer.cs
@@ -3,8 +3,29 @@ namespace TS.TechnicalTest;
 
 public class LongestSentanceAnswer
 {
+    private static readonly char[] separator = new char[] { ' ' };
     public static int Solution(string s)
     {
-        throw new NotImplementedException("Not completed yet");
+
+        // sentence separators
+        char[] sentenceSplit = ['.', '?', '!'];
+        //split using the seperators and remove whie spaces
+        string[] sentences = s.Split(sentenceSplit, StringSplitOptions.RemoveEmptyEntries);
+
+        int maxWordCount = 0;
+        foreach (string sentence in sentences)
+        {
+            //split each sentence to words 
+            string[] words = sentence.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+
+            //check if word contains a letter
+            int wordCount = words.Count(x => x.Any(char.IsLetter));
+
+            if (wordCount > maxWordCount) { maxWordCount = wordCount; }
+        }
+        //return maximum word count
+        return maxWordCount;
+
     }
+
 }
